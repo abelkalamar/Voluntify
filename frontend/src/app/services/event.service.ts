3cbba1ffded182b07cb34b0523629f0bc3cfecb9
Reka Santha-Gosi <gosireka1@gmail.com>
2019-03-28 20:25:11 +0100

    add detailed event view and apply button

--- frontend/src/app/services/event.service.ts
+++ frontend/src/app/services/event.service.ts
@@ -30,4 +30,7 @@ export class EventService {
     console.log('get detailed events');
     // return this.http.get(`${this.baseUrl}/id`, { headers });
   }
+  applyEvent(id:number){
+    console.log('applied');
+  }
 }
