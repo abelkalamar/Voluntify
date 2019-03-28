cd1b526b886ff80f78735218bcaa4da313e0a635
Reka Santha-Gosi <gosireka1@gmail.com>
2019-03-28 21:21:03 +0100

    wire getEvents to backend

--- frontend/src/app/components/main/event-detail/event-detail.component.ts
+++ frontend/src/app/components/main/event-detail/event-detail.component.ts
@@ -30,9 +30,9 @@ export class EventDetailComponent implements OnInit {
 }
 
   ngOnInit() {
-    this.eventService.getEventById(this.eventId);
+    this.eventService.getEventById(this.eventId)
     // .subscribe(event => {
-    // this.event = event;
+    // console.log(event);
     // })
   }
 
