420b0a40f00558b434d62f2bf613915095a62e2e
Reka Santha-Gosi <gosireka1@gmail.com>
2019-03-28 13:48:23 +0100

    base navbar component + fonts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MainComponent } from '../app/components/main/main.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { MainComponent } from './components/main/main.component';
 import { EventListComponent } from './components/main/event-list/event-list.component';
 import { EventDetailComponent } from './components/main/event-detail/event-detail.component';
 import { UpdateProfileComponent } from './components/main/update-profile/update-profile.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/login', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'main', component: MainComponent, children: [
-    {path: 'event/list', component: EventListComponent},
-    {path: 'event/detail', component: EventDetailComponent},
-    {path: 'profile', component: UpdateProfileComponent},
-  ]},
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'main', component: MainComponent, children: [
+      { path: 'event/list', component: EventListComponent },
+      { path: 'event/detail', component: EventDetailComponent },
+      { path: 'profile', component: UpdateProfileComponent },
+    ]
+  },
 ];
 
 @NgModule({
