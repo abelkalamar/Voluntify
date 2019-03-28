c7c53a39392446e185a4a0d4721f478fb7479b89
Reka Santha-Gosi <gosireka1@gmail.com>
2019-03-28 15:07:19 +0100

    add icons and menu to navbar

--- frontend/src/app/components/main/navbar/navbar.component.ts
+++ frontend/src/app/components/main/navbar/navbar.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  selected: string = null;
   constructor() { }
 
   ngOnInit() {
   }
-
+  select(item) {
+    this.selected = item;
+  };
 }
