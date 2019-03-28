7b43b428dccc6162accb3fef254d50298b7a8cce
Reka Santha-Gosi <gosireka1@gmail.com>
2019-03-28 15:41:44 +0100

    add selected menu highlight

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   {
     path: 'main', component: MainComponent, children: [
+      { path: '', component: EventListComponent },
       { path: 'event/list', component: EventListComponent },
       { path: 'event/detail', component: EventDetailComponent },
       { path: 'profile', component: UpdateProfileComponent },
