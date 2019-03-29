00fb3275fdc7fbeb0837d798a77a01d89ca94801
Reka Santha-Gosi <gosireka1@gmail.com>
2019-03-29 10:01:45 +0100

    wired event service to backend

--- frontend/src/app/components/main/event-detail/event-detail.component.ts
+++ frontend/src/app/components/main/event-detail/event-detail.component.ts
@@ -10,13 +10,14 @@ import { EventService } from 'src/app/services/event.service';
 export class EventDetailComponent implements OnInit {
   valid = false;
   eventId: number;
-  event = {id: this.eventId,
-    title: 'First title',
-    start: '2019. 10. 10. 10:00',
-    finish: '2019. 11. 10. 23:00',
-    address: 'Green Fox Academy, Andrássy street',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  };
+  event;
+  // {id: this.eventId,
+  //   title: 'First title',
+  //   start: '2019. 10. 10. 10:00',
+  //   finish: '2019. 11. 10. 23:00',
+  //   address: 'Green Fox Academy, Andrássy street',
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  // };
   constructor(private router: Router, private eventService: EventService) {
     this.router.events.subscribe(value => {
       if (value instanceof NavigationEnd) {
@@ -33,17 +34,20 @@ export class EventDetailComponent implements OnInit {
 }
 
   ngOnInit() {
-    this.eventService.getEventById(this.eventId);
-    // .subscribe(event => {
-    // console.log(event);
-    // })
+    this.eventService.getEventById(this.eventId)
+    .subscribe(event => {
+      this.event = event;
+    console.log(event); //ezt megjeleníteni
+    })
   }
 
   apply() {
-    this.eventService.applyEvent(this.eventId);
-    // .subscribe(res => {
+    this.eventService.applyEvent(this.eventId)
+    .subscribe(res => {
       this.router.navigate(['/main/event/list']);
-    // });
+    }, err => {
+      console.log(err);
+    });
   }
 
 }
